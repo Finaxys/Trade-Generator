83e0ea9163beab24c1b1eab19d1f28a382f3c972
Shinao <rmonnerat.pro@gmail.com>
2015-03-18 19:09:59 +0100

    Fix import

--- src/generals.java
+++ src/generals.java
@@ -1,12 +1,12 @@
-public class generals {
-
-public String nom_de_la_banque;
-public int total_buget; 
-public String owncountry;
-public List<businessunit> bu
-public generals(String nom,int money,String mycountry,List<businessunit> bu){
-this.nom_de la_banque=name;
-this.total_buget=money;
-this.owncountry=mycountry;
-this.bu=bu;
-}
+//public class generals {
+//
+//public String nom_de_la_banque;
+//public int total_buget; 
+//public String owncountry;
+//public List<businessunit> bu
+//public generals(String nom,int money,String mycountry,List<businessunit> bu){
+//this.nom_de la_banque=name;
+//this.total_buget=money;
+//this.owncountry=mycountry;
+//this.bu=bu;
+//}
