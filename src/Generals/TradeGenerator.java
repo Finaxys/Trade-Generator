178b84ff16987e807a384cd564ff1795f4f91abd
Shinao <rmonnerat.pro@gmail.com>
2015-03-20 12:24:31 +0100

    Main loop bu

--- src/Generals/TradeGenerator.java
+++ src/Generals/TradeGenerator.java
@@ -7,35 +7,35 @@ public class TradeGenerator
 	{
 		Referential ref = new Referential();	
 		Generals gen = null;
-		
+
 		gen = LoadXML.init(ref);
-		
-        int simulate_days=Integer.parseInt(args[0]);
-		
+
+		int simulate_days=Integer.parseInt(args[0]);
+
 		int j;
-		
-	for (j=0;j<=simulate_days;j++)
-	{	
-		for (Businessunit bu : gen.bu)
-		{
-			for (Portfolio port :  bu.lpor)
+
+		for (j=0;j<=simulate_days;j++)
+		{	
+			for (Businessunit bu : gen.bu)
 			{
-				for (Book b : port.lb)
+				for (Portfolio port :  bu.lpor)
 				{
-					if (b.name=="equity")
+					for (Book b : port.lb)
 					{
-						Instrument t=bu.lins.getInstrument(b.name);
-						t.generate(gen, ref,gen.total_buget*bu.ratio*por.ratio*b.ratio, j);
-						
-					}  
-					
-					
-					{
-						
+						if (b.name=="equity")
+						{
+							Instrument t= bu.getInstrument(b.name);
+							t.generate(gen, ref, (int) (gen.total_buget*bu.ratio*port.ratio*b.ratio), j);
+
+						}  
+
+
+						{
+
+						}
 					}
 				}
 			}
 		}
-	  }
 	}	
 }
