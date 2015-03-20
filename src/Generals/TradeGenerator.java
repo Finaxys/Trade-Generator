998de6773afe97c4995db06ef10cf376d9ef6a17
vcouvignou <vcouvignou@finaxys.com>
2015-03-20 12:19:29 +0100

    big boucle

--- src/Generals/TradeGenerator.java
+++ src/Generals/TradeGenerator.java
@@ -9,5 +9,33 @@ public class TradeGenerator
 		Generals gen = null;
 		
 		gen = LoadXML.init(ref);
+		
+        int simulate_days=Integer.parseInt(args[0]);
+		
+		int j;
+		
+	for (j=0;j<=simulate_days;j++)
+	{	
+		for (Businessunit bu : gen.bu)
+		{
+			for (Portfolio port :  bu.lpor)
+			{
+				for (Book b : port.lb)
+				{
+					if (b.name=="equity")
+					{
+						Instrument t=bu.lins.getInstrument(b.name);
+						t.generate(gen, ref,gen.total_buget*bu.ratio*por.ratio*b.ratio, j);
+						
+					}  
+					
+					
+					{
+						
+					}
+				}
+			}
+		}
+	  }
 	}	
 }
