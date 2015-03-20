92d0ab5b0efb86cc1c3b22efd8e188b723fa935d
cramo <marc.herault@epitech.eu>
2015-03-20 16:12:26 +0100

    cleaning

--- src/Generals/Equity.java
+++ src/Generals/Equity.java
@@ -1,13 +1,12 @@
 package Generals;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
 
-public class Equity extends Instrument {
-	//public final String type="Equity";
+public class Equity extends Instrument 
+{
 
 	public int Partsell;
 	public int owncountry;
@@ -16,73 +15,53 @@ public class Equity extends Instrument {
 	public double repartition_tolerance;
 	public Boolean is_stp;
 
-	//private Date startDate;
-
-
-
-	public static List<Boolean> tableaubin(int size, int ratio){
-		List<Boolean> t1=new ArrayList<Boolean>(size);
+	public static List<Boolean> tableaubin(int size, int ratio)
+	{
+		List<Boolean> boolArray = new ArrayList<Boolean>(size);
 		int j;
 		int national = (ratio * size) / 100;
 		
 		for (int i = 0; i < size; ++i)
-			t1.add(Boolean.FALSE);
+			boolArray.add(Boolean.FALSE);
 		
-		t1.set(0, Boolean.FALSE);
+		boolArray.set(0, Boolean.FALSE);
 
 		for (j = 0; j <= national; j++)
-			t1.set(j, Boolean.TRUE);
+			boolArray.set(j, Boolean.TRUE);
 
-		Collections.shuffle(t1);
+		Collections.shuffle(boolArray);
 
-		return t1;
+		return (boolArray);
 	}
 
 	@Override
-	public void generate(Generals g,Referential ref, int montant,int date) {
-
-		int montant_total_jour=montant;//remplir avec general
+	public void generate(Generals generals, Referential ref, int amount,int date) 
+	{
+		int amountPerDay = amount;
 		double rand1, rand2;
-		double v;
-		//calcul des  tolérances
-		//exemple 20==>random entre -0.2 et 0.2
-		rand1= this.repartition_tolerance*2*(Math.random()-0.5)/100;
-		rand2=this.volumetry_tolerance*2*(Math.random()-0.5)/100;
-		//montant réel (prise en compte de la tolérance
-		montant_total_jour+=rand1*montant_total_jour;
-		//calcul du nombre de trade a distribuer par jour (entier pair)
-		v=(1-rand2)*volumetry;
-		int volume_arrondi=(int) v;
-		if (volume_arrondi % 2 ==1)
-		{
-			volume_arrondi++;
-		}
-		// Date d=startDate;
-		//DECLARATION des variables de la boucles while  
-		//separer le montant en 2 
-		float rand12;
-		//double random pour la tolérance des quantités 
-		double randomquantity3;
-		double randomquantity4;
-		//montant a diviser chaque jour
-		double to2tradeamount=montant_total_jour/volume_arrondi;
-		//quantité a calculer
-		int quantity1,quantity2;
-		//prix soumis a tolerance randomquantity
-		float price1,price2;		
-		//nombre de jour de simulation en dur
-		//    int day_simulation=number;
-		//    //start date
-		//    int day_start=0;
-		//   //le jour courant
-		//    int numero_jour_courant=0;
-		//les tableaux de distribution de sell/buy; National/international
-		//List<boolean> t1,t2; 
+		double toleredVolumetry;
+
+		rand1 = this.repartition_tolerance * 2 * (Math.random()-0.5)/100;
+		rand2 = this.volumetry_tolerance * 2 * (Math.random()-0.5)/100;
+		amountPerDay += rand1 * amountPerDay;
+		
+		//calculation of number of trades to distribute per day
+		toleredVolumetry = (1-rand2) * volumetry;
+		int roundedVolume = (int) toleredVolumetry;
+		if (roundedVolume % 2 == 1)
+			roundedVolume++;
+ 
+		float randToleranceQuantities;
+		
+		double randomquantity1, randomquantity2;
+		double to2tradeamount = (amountPerDay / roundedVolume);
+		int quantity1, quantity2;
+		float price1,price2;		 
+		
 		List<Boolean> t1;
-
 		List<Boolean> t2;
-
-		//declaration des tirages au sort sous contraintes
+		
+		//declaration des tirages au sort sous contrainte
 		Referential.Depositary d1,d2;
 		Referential.Counterpart c1,c2;
 		Referential.Trader tr1,tr2;
@@ -90,64 +69,48 @@ public class Equity extends Instrument {
 		Referential.Currency cur1,cur2;
 		Referential.Portfolio port1,port2;
 
+		t1 = tableaubin(roundedVolume, this.owncountry);
 
-		//boucle while parcourant les jours de simulation
-		//    while(numero_jour_courant<=day_simulation){
-		//durant un jour:
-		//calcul des tableaux de distribution
+		t2 = tableaubin(roundedVolume, this.Partsell);
+		
+		for (int i = 0; i < roundedVolume; i = i + 2) 
+		{
 
-		t1= tableaubin(volume_arrondi,this.owncountry);
-
-		t2= tableaubin(volume_arrondi,this.Partsell);
-
-		//pour chaque bi-trade
-		int i;
-		for (i=0; i<volume_arrondi; i=i+2) {
-
-			//partage du montantpertrade
-			rand12=(float)Math.random();
+			//sharing of amount per trade
+			randToleranceQuantities = (float)Math.random();
+			
 			//set random price -+3%
-			randomquantity3=6*(Math.random()-0.5);
-			randomquantity4=6*(Math.random()-0.5);
-
+			randomquantity1 = 6 * (Math.random() - 0.5);
+			randomquantity2 = 6 * (Math.random() - 0.5);
 			//tirage au sort sous contrainte
 
-			d1=ref.getRandomElement(ref.Depositaries);
-			d2=ref.getRandomElement(ref.Depositaries);
-			c1=ref.getRandomElement(ref.Counterparts);
-			c2=ref.getRandomElement(ref.Counterparts);
-			tr1=ref.getRandomElement(ref.Traders);
-			tr2=ref.getRandomElement(ref.Traders);
-			pro1=ref.getRandomElement(ref.Products);
-			pro2=ref.getRandomElement(ref.Products);
-			cur1=ref.getRandomElement(ref.Currencies);
-			cur2=ref.getRandomElement(ref.Currencies);
-			port1=ref.getRandomElement(ref.Portfolios);
-			port2=ref.getRandomElement(ref.Portfolios);
+			d1 = ref.getRandomElement(ref.Depositaries);
+			d2 = ref.getRandomElement(ref.Depositaries);
+			c1 = ref.getRandomElement(ref.Counterparts);
+			c2 = ref.getRandomElement(ref.Counterparts);
+			tr1 = ref.getRandomElement(ref.Traders);
+			tr2 = ref.getRandomElement(ref.Traders);
+			pro1 = ref.getRandomElement(ref.Products);
+			pro2 = ref.getRandomElement(ref.Products);
+			cur1 = ref.getRandomElement(ref.Currencies);
+			cur2 = ref.getRandomElement(ref.Currencies);
+			port1 = ref.getRandomElement(ref.Portfolios);
+			port2 = ref.getRandomElement(ref.Portfolios);
 
+			price1 = pro1.price;
+			price2 = pro2.price;
+			price1 = (float) (price1 * ( 1 + randomquantity1 / 100));
+			price2 = (float) (price2 * (1 + randomquantity2 / 100));
+			quantity1 = (int) (randToleranceQuantities * to2tradeamount / price1);
+			quantity2 = (int) (randToleranceQuantities * to2tradeamount / price2);
 
-			price1=pro1.price;
-			price2=pro2.price;
-			price1=(float) (price1*(1+randomquantity3/100));
-			price2=(float) (price2*(1+randomquantity4/100));
-			quantity1=(int) (rand12*to2tradeamount/price1);
-			quantity2=(int) (rand12*to2tradeamount/price2);
-
-
-			Tradeequity tq1= new Tradeequity(date,t2.get(i),price1,quantity1,d1,c1,tr1,pro1,cur1,port1);
-			Tradeequity tq2= new Tradeequity(date,t2.get(i+1),price2,quantity2,d2,c2,tr2,pro2,cur2,port2);
+			Tradeequity tq1 = new Tradeequity(date, t2.get(i), price1, quantity1, d1, c1, tr1, pro1, cur1, port1);
+			Tradeequity tq2 = new Tradeequity(date, t2.get(i + 1), price2, quantity2, d2, c2, tr2, pro2, cur2, port2);
 			
 			System.out.println("Equity >> " + date + " / " + tq1.prix + " / " + tq1.quantity + " / " + tq1.port1.codeptf);
 
-			// Tradeequity tq2=
-			g.addTradevent(tq1);
-			g.addTradevent(tq2);
+			generals.addTradevent(tq1);
+			generals.addTradevent(tq2);
 		}
-
-		//montant_total_jour += montant_total_jour*;
-
-
-		//}
-
 	}
 }
--- src/Generals/Referential.java
+++ src/Generals/Referential.java
@@ -1,18 +1,7 @@
 package Generals;
 
-import java.io.File;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
-
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
-
 
 class Referential
 {
