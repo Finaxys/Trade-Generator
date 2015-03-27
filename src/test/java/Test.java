50dc63f4357f771f2c2d031796d28ef68ca50a4f
cramo <marc.herault@epitech.eu>
2015-03-27 14:44:46 +0100

    test

--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -1,5 +1,16 @@
+import junit.framework.TestCase;
 
-public class test
+public class Test extends TestCase
 {
-
+	public Test(String testMethodName)
+	{
+		super(testMethodName);
+	}
+	
+	public void test() throws Exception
+	{
+		fail("test à échouer");
+	}
 }
+//chargement des settings 
+//création de trades
