f9c30b113fce188c44aa0c12cee1fa9058acbed4
Shinao <rmonnerat.pro@gmail.com>
2015-04-02 16:51:25 +0200

    Norme trade generator main

--- src/main/java/Generals/TradeGenerator.java
+++ src/main/java/Generals/TradeGenerator.java
@@ -8,6 +8,7 @@ public class TradeGenerator
 {
 	public static void main(String[] args)
 	{
+		// Stat
 		long startTime = System.currentTimeMillis();
 
 		Referential ref = Referential.getInstance();
@@ -40,6 +41,7 @@ public class TradeGenerator
 			for (Businessunit bu : gen.bu)
 			{
 				dis = bu.getMainInstrumentCount();
+
 				for (Portfolio port : bu.getPortfolios())
 				{
 
@@ -75,26 +77,13 @@ public class TradeGenerator
 			OutputManager.getInstance().outputTrades();
 		}
 
-		long estimatedTime = System.currentTimeMillis() - startTime;
-		System.out.println((float) estimatedTime * 100000 / 1000 / 60 / 60);
-		System.out.println("Done");
 		Report.ConcatSortOutput();
 //		Report.report(Report.liste, simulate_days);
 //		System.out.println("Report done");
-	}
 
-	static void writeXMLNode(PrintWriter writer, TradeEvent.Node node)
-	{
-		// Check if there is nodes inside node -> recursion
-		if (node.nodes != null)
-		{
-			writer.write("<" + node.name + ">" + System.lineSeparator());
-			for (TradeEvent.Node n : node.nodes)
-				writeXMLNode(writer, n);
-			writer.write("</" + node.name + ">" + System.lineSeparator());
-		}
-		// Only simple node -> print it
-		else
-			writer.write("<" + node.name + ">" + node.value + "</" + node.name + ">" + System.lineSeparator());
+		// Estimation Stats
+		long estimatedTime = System.currentTimeMillis() - startTime;
+		System.out.println((float) estimatedTime * 100000 / 1000 / 60 / 60);
+		System.out.println("Done");
 	}
 }
