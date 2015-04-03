fb57259550ebb279892a60e723413ac6a0b26988
zghezal <ghezal.id@gmail.com>
2015-04-03 15:18:47 +0200

    report

--- src/main/java/Generals/Report.java
+++ src/main/java/Generals/Report.java
@@ -7,6 +7,7 @@ import java.io.PrintWriter;
 import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Date;
 public class Report {
 
 
@@ -55,13 +56,13 @@ public static void ConcatSortOutput(){
 	Collections.sort(Report.liste);
 }	
 
-public static void writeCSTrade(TradeEvent te, int nombre_op,float montant) throws FileNotFoundException, UnsupportedEncodingException
+public static void writeCSTrade(TradeEvent te, int nombre_op,double montant) throws FileNotFoundException, UnsupportedEncodingException
 {
 	
 	String s=System.lineSeparator();
 	
 
-	writer.write(te.getDate() +","+te.getBook().getPortFolios().getBu().getName()+","+te.getBook().getPortFolios().getName() +","+te.getBook().getName() +","+te.getInstrument()+","+te.getWay() +","+nombre_op+","+montant +","+ s);
+	writer.write(printDate(te.getDate()) +","+te.getBook().getPortFolios().getBu().getName()+","+te.getBook().getPortFolios().getName() +","+te.getBook().getName() +","+te.getInstrument().getName()+","+te.getWay() +","+nombre_op+","+montant +","+ s);
 //	writer.write(System.lineSeparator());
 //	writer.write(te.date.toString() +",");
 //	writer.write(System.lineSeparator());
@@ -91,6 +92,9 @@ public static void writeCSTrade(TradeEvent te, int nombre_op,float montant) thro
 
 	
 }
+public static String printDate(Date d){
+	return d.getDate()+"-"+d.getMonth()+"-"+d.getYear();
+}
 public static void report(ArrayList<TradeEvent> lists,int j){
 	
 	File check = new File(OUTPUT_PATH );
@@ -106,19 +110,18 @@ public static void report(ArrayList<TradeEvent> lists,int j){
 		TradeEvent te;
 		te=lists.get(0);
 		int nombre_transaction=1;
-		int ntt=1;
-		float MS=0;
-		float ME=0;
+		double MS=0;
+		double ME=0;
 		int NE=0;
 		int NS=0;
-		float montant=te.amount;
+		double montant=te.getAmount();
 		int i=0;
 		
 		while (i<lists.size())
 		{   	
 			if (i==lists.size()-1)
 			{
-			montant=montant+lists.get(i).amount;
+			montant=montant+lists.get(i).getAmount();
 			nombre_transaction++;
 			writeCSTrade(te,nombre_transaction,montant);
 
@@ -127,14 +130,14 @@ public static void report(ArrayList<TradeEvent> lists,int j){
 			{
 				if (lists.get(i).compareTo(te)==0)
 				{	
-					montant=montant+lists.get(i).amount;
+					montant=montant+lists.get(i).getAmount();
 					nombre_transaction++;
 				}else
 				{	writeCSTrade(te,nombre_transaction,montant);
 					
 					if (!lists.get(i).getWay().name().equalsIgnoreCase("sell")) {MS=MS+montant;NS=NS+nombre_transaction;}else{ME=ME+montant;NE=NE+nombre_transaction;}
 					nombre_transaction=1;
-					montant=lists.get(i).amount; 
+					montant=lists.get(i).getAmount();
 					
 				}
 				
