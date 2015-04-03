1680021ef1e0800587ac20b86bbfc0f9f73afab4
cramo <marc.herault@epitech.eu>
2015-04-03 12:12:46 +0200

    norm EquityGenerator

--- src/main/java/Generals/EquityGenerator.java
+++ src/main/java/Generals/EquityGenerator.java
@@ -4,54 +4,44 @@ import java.util.Date;
 import java.util.List;
 import java.util.Random;
 
-import Generals.Referential.Counterpart;
-import Generals.Referential.Depositary;
-import Generals.Referential.Portfolio;
-import Generals.Referential.Product;
-import Generals.Referential.Trader;
-import Generals.TradeEvent.Node;
-
 public class EquityGenerator extends TradeGenerator
 {
-	private int 		partSell;
-	private int 		ownCountry;
-	private int 		volumetry;
-	private double 		volumetryTolerance;
-	private double 		repartitionTolerance;
-	private Boolean 	isStp;
-	private int  roundedVolumetry;
-	private List<Integer> Loanpertrade ;
-	private List<Locality> t1;
-	private List<Way> t2;
-	private int amountPerDay;
+	private int 			partSell;
+	private int 			ownCountry;
+	private int 			volumetry;
+	private double 			volumetryTolerance;
+	private double 			repartitionTolerance;
+	private Boolean 		isStp;
+	private int  			roundedVolumetry;
+	private List<Integer> 	Loanpertrade ;
+	private List<Locality> 	localities;
+	private List<Way> 		ways;
+	private int 			amountPerDay;
 	
 	@Override
 	public void init(int amount)
 	{
 
 		amountPerDay = amount;
-		double rand1, rand2;
+		double rdmRepartionTolerance;
+		double rdmVolumetryTolerance;
 		double toleredVolumetry;
 		Random random = new Random();
 
-		rand1 = this.getRepartitionTolerance() * 2 * (random.nextDouble() - 0.5)
-				/ 100;
-		rand2 = this.getVolumetryTolerance() * 2 * (random.nextDouble() - 0.5) / 100;
+		rdmRepartionTolerance = this.getRepartitionTolerance() * 2 * (random.nextDouble() - 0.5) / 100;
+		rdmVolumetryTolerance = this.getVolumetryTolerance() * 2 * (random.nextDouble() - 0.5) / 100;
 
-		amountPerDay += rand1 * amountPerDay;
+		amountPerDay += rdmRepartionTolerance * amountPerDay;
 
 		// calculation of number of trades to distribute per day
-		toleredVolumetry = (1 - rand2) * volumetry;
-		 this.roundedVolumetry = (int) toleredVolumetry;
+		toleredVolumetry = (1 - rdmVolumetryTolerance) * volumetry;
+		this.roundedVolumetry = (int) toleredVolumetry;
 
+		Loanpertrade = Sparsemoney(roundedVolumetry, amountPerDay);
 
- Loanpertrade = Sparsemoney(roundedVolumetry, amountPerDay);
+		localities = TradeGenerator.tableaubin(roundedVolumetry, this.getOwnCountry(), Locality.class);
+		ways = TradeGenerator.tableaubin(roundedVolumetry, this.getPartSell(), Way.class);
 
-
-		t1 = TradeGenerator.tableaubin(roundedVolumetry,
-				 this.getOwnCountry(),Locality.class);
-				 t2 = TradeGenerator.tableaubin(roundedVolumetry, this.getPartSell(),Way.class);
-				
 	}
 
 	@Override
@@ -61,93 +51,100 @@ public class EquityGenerator extends TradeGenerator
 		Generals generals = Generals.getInstance();
 
 		// declaration des tirages au sort sous contrainte
-		Referential.Depositary d;
-		Referential.Counterpart c;
-		Referential.Trader tr;
-		Referential.Product pro;
-		Referential.Currency cur = null;
-		Referential.Portfolio port;
-		
-		 List<Referential.Product> Listequity=ref.subList(ref.products,
-		 "type", "EQUITY");
-		 t1 = TradeGenerator.tableaubin(this.roundedVolumetry,
-		 this.getOwnCountry(),Locality.class);
-		 t2 = TradeGenerator.tableaubin(roundedVolumetry, this.getPartSell(),Way.class);
+		Referential.Depositary depositary;
+		Referential.Counterpart counterpart;
+		Referential.Trader trader;
+		Referential.Product product;
+		Referential.Currency currency = null;
+		Referential.Portfolio portfolio;
 
+		List<Referential.Product> Listequity = ref.subList(ref.products, "type", "EQUITY");
+		localities = TradeGenerator.tableaubin(this.roundedVolumetry, this.getOwnCountry(), Locality.class);
+		ways = TradeGenerator.tableaubin(roundedVolumetry, this.getPartSell(), Way.class);
 
-		 // tirage au sort sous contrainte
+		// tirage au sort sous contrainte
 
-		 d = ref.getRandomElement(ref.depositaries);
-		 c = ref.getRandomElement(ref.counterparts);
-		 cur = ref.getRandomElement(ref.currencies);
-		 tr = ref.getTrader(ref, cur.country, "equity");
-		 if (t1.get(0).toString() == "NATIONAL")
-		 {
-			 cur = ref.subList(ref.currencies, "country", generals.owncountry).get(0);
-		 }
-		 {
-			 cur = ref.getRandomElement(ref.exList(ref.currencies, "country", generals.owncountry));
-		 }
-		 pro = ref.getRandomElement(Listequity);
-		 port = ref.getRandomElement(ref.portfolios);
-		 
-		 float randToleranceQuantities;
+		depositary = ref.getRandomElement(ref.depositaries);
+		counterpart = ref.getRandomElement(ref.counterparts);
+		currency = ref.getRandomElement(ref.currencies);
+		trader = ref.getTrader(ref, currency.country, "equity");
+		if (localities.get(0).toString() == "NATIONAL")
+		{
+			currency = ref.subList(ref.currencies, "country", generals.owncountry).get(0);
+		}
+		{
+			currency = ref.getRandomElement(ref.exList(ref.currencies, "country", generals.owncountry));
+		}
+		product = ref.getRandomElement(Listequity);
+		portfolio = ref.getRandomElement(ref.portfolios);
 
-		 double randomquantity;
-		 // sharing of amount per trade
-		 randToleranceQuantities = (float) Math.random();
-		 Random random = new Random();
-		 // set random price -+3%
-		 randomquantity = 6 * (random.nextDouble() - 0.5);
-		 float price = pro.price;
-		 // price=price*1/cur.change*Refential.Currency.this.getCurrencybycountry("country").change;
+		float randToleranceQuantities;
 
-		 price = (float) (price * (1 + randomquantity / 100));
-		 int quantity = (int) (randToleranceQuantities * Loanpertrade.get(0) / price);
-		 TradeEquity tq1 = new TradeEquity(this, "reference",t2.get(0), date, date, c, book, price,quantity,pro,d,tr);
-		 tradeGenerated(tq1);
-		 t1.remove(0);
-		 t2.remove(0);
-		 Loanpertrade.remove(0);
+		double randomquantity;
+		// sharing of amount per trade
+		randToleranceQuantities = (float) Math.random();
+		Random random = new Random();
+		// set random price -+3%
+		randomquantity = 6 * (random.nextDouble() - 0.5);
+		float price = product.price;
+		// price=price*1/cur.change*Refential.Currency.this.getCurrencybycountry("country").change;
+
+		price = (float) (price * (1 + randomquantity / 100));
+		int quantity = (int) (randToleranceQuantities * Loanpertrade.get(0) / price);
+		TradeEquity tradeEquity = new TradeEquity(this, "reference", ways.get(0), date, date, counterpart, book, price, quantity, product, depositary, trader);
+		tradeGenerated(tradeEquity);
+		localities.remove(0);
+		ways.remove(0);
+		Loanpertrade.remove(0);
 	}
 
-	public int getOwnCountry() {
+	public int getOwnCountry()
+	{
 		return ownCountry;
 	}
 
-	public void setOwnCountry(int ownCountry) {
+	public void setOwnCountry(int ownCountry)
+	{
 		this.ownCountry = ownCountry;
 	}
 
-	public int getPartSell() {
+	public int getPartSell()
+	{
 		return partSell;
 	}
 
-	public void setPartSell(int partSell) {
+	public void setPartSell(int partSell)
+	{
 		this.partSell = partSell;
 	}
 
-	public double getRepartitionTolerance() {
+	public double getRepartitionTolerance()
+	{
 		return repartitionTolerance;
 	}
 
-	public void setRepartitionTolerance(double repartitionTolerance) {
+	public void setRepartitionTolerance(double repartitionTolerance)
+	{
 		this.repartitionTolerance = repartitionTolerance;
 	}
 
-	public int getVolumetry() {
+	public int getVolumetry()
+	{
 		return volumetry;
 	}
 
-	public void setVolumetry(int volumetry) {
+	public void setVolumetry(int volumetry)
+	{
 		this.volumetry = volumetry;
 	}
 
-	public double getVolumetryTolerance() {
+	public double getVolumetryTolerance()
+	{
 		return volumetryTolerance;
 	}
 
-	public void setVolumetryTolerance(double volumetryTolerance) {
+	public void setVolumetryTolerance(double volumetryTolerance)
+	{
 		this.volumetryTolerance = volumetryTolerance;
 	}
 }
