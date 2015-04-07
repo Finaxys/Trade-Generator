3ea650ec63f13bcd736414c823050538d0de0d70
cramo <marc.herault@epitech.eu>
2015-04-07 15:26:59 +0200

    fixed test

--- src/test/java/ReferentialTest.java
+++ src/test/java/ReferentialTest.java
@@ -3,39 +3,36 @@ import java.util.ArrayList;
 import Generals.Generator;
 import Generals.Referential;
 import Generals.Referential.Trader;
-import Generals.TradeGenerator;
 import junit.framework.TestCase;
-
 
 public class ReferentialTest extends TestCase
 {
 	private static Referential ref = Referential.getInstance();
-	
-	
+
 	public static void testGetRandomElement()
 	{
-		ArrayList<Integer>l = new ArrayList<Integer>();
+		ArrayList<Integer> l = new ArrayList<Integer>();
 		l.add(11);
 		l.add(12);
 		l.add(13);
 		int result = ref.getRandomElement(l);
 		System.out.println(result);
 	}
-	
+
 	public static void testGetTrader()
 	{
 		String instrument = new String("equity");
-		String country = new String("CN");
+		String code = new String("EUR");
 
-		Generator.main(new String[] {"10"});
-		Trader trader = ref.getTrader(ref, country, instrument);
+		Generator.main(new String[] { "10" });
+		Trader trader = ref.getTrader(ref, code, instrument);
 		if (trader == null)
 			System.out.println("trader is null");
-		System.out.println("name trader : " + trader.name);
-		System.out.println("codeptf trader : " + trader.codeptf);
-		
-		
+		else
+		{
+			System.out.println("name trader : " + trader.name);
+			System.out.println("codeptf trader : " + trader.codeptf);
+		}
 
-		
 	}
 }
